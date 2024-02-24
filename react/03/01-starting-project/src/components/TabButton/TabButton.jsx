e44f06b178e45b3f66a8289d3ead8127532dcfd7
seungwonme <seungwonan.me@gmail.com>
2024-02-24 15:31:23 +0900

    Finish 03

---
+++ react/03/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,16 @@
+import "./TabButton.css";
+
+function TabButton({ children, onSelect, isSelected }) {
+	/* javascript event listener
+	document.querySelector("menu").addEventListener("click", () => {
+		console.log("clicked");
+	}); */
+	return (
+		<li>
+			{/* children props is used to access the content between the opening and closing tags of the component */}
+			<button className={isSelected ? "active" : ""} onClick={onSelect}>{children}</button>
+		</li>
+	);
+}
+
+export default TabButton;
