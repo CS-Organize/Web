78e831d071f4a9d5ffe33f077c89d9cb726bc9b5
seungwonme <seungwonan.me@gmail.com>
2024-02-24 16:24:34 +0900

    Move the 03/01-starting-project to 01-starting-project and divide the project into components

--- react/01-starting-project/src/components/TabButton.jsx
+++ react/01-starting-project/src/components/TabButton.jsx
@@ -1,10 +1,9 @@
-import "./TabButton.css";
-
 function TabButton({ children, onSelect, isSelected }) {
 	/* javascript event listener
 	document.querySelector("menu").addEventListener("click", () => {
 		console.log("clicked");
 	}); */
+	console.log("TabButton is rendered.");
 	return (
 		<li>
 			{/* children props is used to access the content between the opening and closing tags of the component */}
